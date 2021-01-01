18e84f05c3a231c4904ebe9197bb78f96d47d272
Gary Tokman <hello@garytokman.me>
2021-01-01 15:41:45 -0400

    update hybrid cam view with defaults

--- Sources/HybridCamera/HybridCamView/HybridCamView+Create.swift
+++ Sources/HybridCamera/HybridCamView/HybridCamView+Create.swift
@@ -1,40 +1,54 @@
 import UIKit
 import With
+import ZoomSwitcherKit
 /**
  * Create
  */
 extension HybridCamView {
-   /**
-    * Creates camView
-    */
-   @objc open func createCamView() -> CamView {
-      let rect: CGRect = .init(origin: .zero, size: UIScreen.main.bounds.size)
-      return with(.init(frame: rect)) {
-         self.addSubview($0)
-      }
-   }
-   /**
-    * Creates topBar
-    */
-   @objc open func createTopBar() -> TopBarViewKind {
-      fatalError("must be overriden in subclass")
-   }
-   /**
-    * Creates Record button
-    */
-   @objc open func createRecordButton() -> RecordButtonViewKind {
-      fatalError("must be overriden in subclass")
-   }
-   /**
-    * Creates zoom switcher
-    */
-//   @objc open func createZoomSwitchBtn() -> ZoomSwitchBtnKind {
-//      fatalError("must be overriden in subclass")
-//   }
-   /**
-    * Creates zoom switcher
-    */
-   @objc open func createZoomSwitcher() -> ZoomSwitcherKind {
-      fatalError("must be overriden in subclass")
-   }
+    /**
+     * Creates camView
+     */
+    @objc open func createCamView() -> CamView {
+        let rect: CGRect = .init(origin: .zero, size: UIScreen.main.bounds.size)
+        return with(.init(frame: rect)) {
+            self.addSubview($0)
+        }
+    }
+    /**
+     * Creates topBar
+     */
+    @objc open func createTopBar() -> TopBarViewKind {
+        with(TopBar(frame: TopBar.rect)) {
+            self.addSubview($0)
+        }
+    }
+    /**
+     * Creates Record button
+     */
+    @objc open func createRecordButton() -> RecordButtonViewKind {
+        with(RecordButton(frame: RecordButton.rect)) {
+            self.addSubview($0)
+        }
+    }
+    /**
+     * Creates zoom switcher
+     */
+    //   @objc open func createZoomSwitchBtn() -> ZoomSwitchBtnKind {
+    //      fatalError("must be overriden in subclass")
+    //   }
+    /**
+     * Creates zoom switcher
+     */
+    @objc open func createZoomSwitcher() -> ZoomSwitcherKind {
+        let backCamType: BackCameraType = .backCameraType
+        let size = ZoomSwitcher.getSize(backCamType: backCamType)
+        let rect = ZoomSwitcher.getRect(size: size) // .init(origin: .zero, size: size
+        return with(ZoomSwitcher(frame: rect, backCameraType: backCamType)) {
+            $0.buttons[backCamType.defaultLenseIndex].toggle = true // for tripple cam this needs to be .second,
+            //         $0.onSwitch = { focalType in
+            //            Swift.print("Switched to focalType:  \(focalType)")
+            //         }
+            self.addSubview($0)
+        }
+    }
 }
